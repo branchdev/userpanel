e161c11432b13e331deef81bf38f9f88e81485fc
Mike Branchini <mikebranchini@gmail.com>
2013-06-24 17:51:25 -0700

    created project

---
+++ userpanel/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Userpanel::Application.config.secret_token = '20b5c65a0237ec15b069714be03bb837d75caf42bd5c39c91fbf84bebd221861a23f5d133c183aa1a2ab98ade317c3cff7a88d7e144b98e933dac50986a6b827'
